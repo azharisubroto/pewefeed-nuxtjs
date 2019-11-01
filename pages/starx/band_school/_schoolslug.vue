a21241115bc8dd52466b43ede4e351b79185b8ee
Riza Alif Wildani <rizaalifdmc@gmail.com>
2019-11-01 14:59:16 +0700

    Single Band n School

---
+++ pages/starx/band_school/_schoolslug.vue
@@ -0,0 +1,18 @@
+<template>
+  <StarxSingleSekolah />
+</template>
+
+<script>
+import StarxSingleSekolah from '@/components/starx/StarxSingleSekolah';
+
+export default {
+  components: {
+    StarxSingleSekolah,
+  },
+  data(){
+      return{
+          reactive: true,
+      }
+  },
+};
+</script>
