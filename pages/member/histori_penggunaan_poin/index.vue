fe69d93e5b615c5b9c1591de0769148829975126
Riza Alif Wildani <rizaalifdmc@gmail.com>
2019-11-20 15:07:11 +0700

    Fixed histori penggunaan poin refresh

--- pages/member/histori_penggunaan_poin/index.vue
+++ pages/member/histori_penggunaan_poin/index.vue
@@ -209,7 +209,7 @@ export default {
         return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'K' : Math.sign(num)*Math.abs(num)
     },
   },
-  created() {
+  mounted() {
     this.fetchUserdata()
     this.fethMutasi()
   }
