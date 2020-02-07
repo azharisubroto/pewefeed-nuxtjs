4041e4ab6e44cc0291e966eb01047d34ef874aed
Azhari Subroto <juarathemes@gmail.com>
2020-02-07 15:52:32 +0700

    task 13 design update

--- components/member/midtrans/Pending.vue
+++ components/member/midtrans/Pending.vue
@@ -1,160 +1,165 @@
 <template>
-	<section class="pending-page">
-		<v-container>
-			<v-row v-if="list">
-				<v-col>
-					<v-expansion-panels v-if="!loading && datamidtrans" class="mb-3" focusable>
-                        <v-expansion-panel
-                        v-for="(mid, i) in datamidtrans"
-                        :key="i"
-                        class="mb-3"
-                        >
-                            <v-expansion-panel-header>
-                                <v-row no-gutters>
-                                    <v-col cols="5 text--secondary">{{ '#' + mid.order_id }}</v-col>
-                                    <v-col cols="" class="text-center">{{ mid.created_at }}</v-col>
-                                </v-row>
-                            </v-expansion-panel-header>
+  <section class="pending-page">
+    <v-container>
+      <v-row v-if="list">
+        <v-col>
+          <v-expansion-panels v-if="!loading && datamidtrans" class="mb-3" focusable>
+            <v-expansion-panel v-for="(mid, i) in datamidtrans" :key="i" class="mb-3">
+              <v-expansion-panel-header>
+                <v-row no-gutters>
+                  <v-col cols="4" class="text--secondary caption">{{ mid.created_at }}</v-col>
+                  <v-col
+                    cols="8"
+                  >Pembayaran dengan No. {{ '#' + mid.expired_at }} akan berakhir pada {{ mid.expired_at }}</v-col>
+                </v-row>
+              </v-expansion-panel-header>
 
-                            <!-- === if xendit === -->
-                            <v-expansion-panel-content class="pt-3" v-if="mid.xendit">
-                                <v-subheader>Detail Pembayaran</v-subheader>
-                                <v-divider></v-divider>
-                                <v-container>
-                                    <v-btn @click="openIframe(mid.invoice_url)" small block color="success">Lihat</v-btn>
-                                </v-container>
-                            </v-expansion-panel-content>
+              <!-- === if xendit === -->
+              <v-expansion-panel-content class="pt-3" v-if="mid.xendit">
+                <v-subheader>Detail Pembayaran</v-subheader>
+                <v-divider></v-divider>
+                <v-btn @click="openIframe(mid.invoice_url)" block color="deep-orange" dark>Lihat</v-btn>
+              </v-expansion-panel-content>
 
-                            <!-- === if midtrans === -->
-                            <v-expansion-panel-content class="pt-3" v-else>
-                                <v-subheader>Detail Pembayaran</v-subheader>
-                                <v-divider></v-divider>
-                                <v-simple-table>
-                                    <template v-slot:default>
-                                    <tbody>
-                                        <tr>
-                                            <td>Transfer Ke</td>
-                                            <td>BCA</td>
-                                        </tr>
-                                        <tr v-if="mid.veritrans_callback.va_numbers.length > 0">
-                                            <td>No. Virtual Account</td>
-                                            <td>
-                                                <v-row no-gutters v-for="(va, i) in mid.veritrans_callback.va_numbers" :key="i">
-                                                    <v-col cols="12">
-                                                        {{va.va_number}}
-                                                    </v-col>
-                                                </v-row>
-                                            </td>
-                                        </tr>
-                                        <tr v-else>
-                                            <td>No. Virtual Account</td>
-                                            <td> - </td>
-                                        </tr>
-                                        <tr>
-                                            <td>Nama Perusahan</td>
-                                            <td>PT. Jayadata Indonesia</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Jumlah Transfer</td>
-                                            <td>{{'Rp.' + mid.amount + ',-'}}</td>
-                                        </tr>
-                                    </tbody>
-                                    </template>
-                                </v-simple-table>
-                                <v-card>
-                                    <v-card-text>
-                                        <v-row>
-                                            <v-col cols="12">
-                                                <v-alert
-                                                    prominent
-                                                    text
-                                                    type="info"
-                                                    success>Informasi Penting</v-alert>
-                                            </v-col>
-                                            <v-col cols="12">
-                                                <ul>
-                                                    <li>Order ini akan expired pada {{mid.expired_at}}</li>
-                                                    <li>Jika anda telah melakukan pembayaran, secara otomatis transaksi ini akan berubah menjadi "Selesai" tanpa perlu konfirmasi dari anda</li>
-                                                    <li>Untuk membatalkan transaksi ini cukup dengan tidak melakukan pembayaran</li>
-                                                </ul>
-                                            </v-col>
-                                        </v-row>
-                                    </v-card-text>
-                                </v-card>
-                            </v-expansion-panel-content>
+              <!-- === if midtrans === -->
+              <v-expansion-panel-content class="pt-3" v-else>
+                <v-subheader>Detail Pembayaran</v-subheader>
+                <v-divider></v-divider>
+                <v-simple-table>
+                  <template v-slot:default>
+                    <tbody>
+                      <tr>
+                        <td>Transfer Ke</td>
+                        <td>BCA</td>
+                      </tr>
+                      <tr v-if="mid.veritrans_callback.va_numbers.length > 0">
+                        <td>No. Virtual Account</td>
+                        <td>
+                          <v-row
+                            no-gutters
+                            v-for="(va, i) in mid.veritrans_callback.va_numbers"
+                            :key="i"
+                          >
+                            <v-col cols="12">{{va.va_number}}</v-col>
+                          </v-row>
+                        </td>
+                      </tr>
+                      <tr v-else>
+                        <td>No. Virtual Account</td>
+                        <td>-</td>
+                      </tr>
+                      <tr>
+                        <td>Nama Perusahan</td>
+                        <td>PT. Jayadata Indonesia</td>
+                      </tr>
+                      <tr>
+                        <td>Jumlah Transfer</td>
+                        <td>{{'Rp.' + mid.amount + ',-'}}</td>
+                      </tr>
+                    </tbody>
+                  </template>
+                </v-simple-table>
+                <v-card>
+                  <v-card-text>
+                    <v-row>
+                      <v-col cols="12">
+                        <v-alert prominent text type="info" success>Informasi Penting</v-alert>
+                      </v-col>
+                      <v-col cols="12">
+                        <ul>
+                          <li>Order ini akan expired pada {{mid.expired_at}}</li>
+                          <li>Jika anda telah melakukan pembayaran, secara otomatis transaksi ini akan berubah menjadi "Selesai" tanpa perlu konfirmasi dari anda</li>
+                          <li>Untuk membatalkan transaksi ini cukup dengan tidak melakukan pembayaran</li>
+                        </ul>
+                      </v-col>
+                    </v-row>
+                  </v-card-text>
+                </v-card>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+          <br />
+          <br />
+          <br />
+          <br />
+        </v-col>
+      </v-row>
+      <v-row v-else-if="!list && !loading">
+        <v-col>
+          <v-alert prominent text type="info" success>Tidak ada data yang tersedia</v-alert>
+        </v-col>
+      </v-row>
+      <v-skeleton-loader
+        v-else
+        class="mx-auto mt-5"
+        type="list-item-avatar-three-line, list-item-avatar-three-line, list-item-avatar-three-line"
+      ></v-skeleton-loader>
+    </v-container>
 
-                        </v-expansion-panel>
-                    </v-expansion-panels>
-					<br>
-					<br>
-					<br>
-					<br>
-				</v-col>
-			</v-row>
-			<v-row v-else-if="!list && !loading">
-				<v-col>
-					<v-alert
-					prominent
-					text
-					type="info"
-					success>Tidak ada data yang tersedia</v-alert>
-				</v-col>
-			</v-row>
-			<v-skeleton-loader v-else
-			class="mx-auto mt-5"
-			type="list-item-avatar-three-line, list-item-avatar-three-line, list-item-avatar-three-line"
-			></v-skeleton-loader>
-		</v-container>
-        
-        <IframePreview :dialogVisible="iframeDialogVisible" :invoiceUrl="invoiceUrl" @close="iframeClose()"/>
-	</section>
+    <IframePreview
+      :dialogVisible="iframeDialogVisible"
+      :invoiceUrl="invoiceUrl"
+      @close="iframeClose()"
+    />
+  </section>
 </template>
 <script>
-import UserService from '@/services/UserService'
+import UserService from "@/services/UserService";
 import IframePreview from "@/components/modal/IframePreview";
 export default {
-	name:"Pending",
-	data() {
-		return {
-            midtranscallback: [],
-            datamidtrans: [],
-            list: false,
-            loading: true,
-            iframeDialogVisible: false,
-            invoiceUrl: ''
-		}
-    },
-    components: {
-        IframePreview
-    },
-	methods: {
-		async fetchData() {
-            try {
-                const res = await UserService.statusTransfer()
-                if (res.data.data.pending.length > 0) {
-                    this.list = true
-                }
-                this.loading = false
-                console.log(res)
-                this.datamidtrans = res.data.data.pending
-            } catch (error) {
-                console.log(error)
-            }
-        },
-        iframeClose () {
-            this.iframeDialogVisible = false
-        },
-        openIframe(url) {
-            this.invoiceUrl = url
-            this.iframeDialogVisible = true
+  name: "Pending",
+  data() {
+    return {
+      midtranscallback: [],
+      datamidtrans: [],
+      list: false,
+      loading: true,
+      iframeDialogVisible: false,
+      invoiceUrl: ""
+    };
+  },
+  components: {
+    IframePreview
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const res = await UserService.statusTransfer();
+        if (res.data.data.pending.length > 0) {
+          this.list = true;
         }
-	},
-	mounted() {
-		this.fetchData()
-	}
-}
+        this.loading = false;
+        this.datamidtrans = res.data.data.pending;
+        console.log(JSON.parse(JSON.stringify(this.datamidtrans)));
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    iframeClose() {
+      this.iframeDialogVisible = false;
+    },
+    openIframe(url) {
+      this.invoiceUrl = url;
+      this.iframeDialogVisible = true;
+    }
+  },
+  mounted() {
+    this.fetchData();
+  }
+};
 </script>
 <style lang="sass" scoped>
 	.pending-page .container
 		background: #f9f9f9
+</style>
+<style lang="scss">
+.finish-page .container {
+  background: #f9f9f9;
+}
+.pending-page .v-icon {
+  color: #ff5722 !important;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
 </style>
--- components/member/midtrans/Sukses.vue
+++ components/member/midtrans/Sukses.vue
@@ -1,144 +1,152 @@
 <template>
-	<section class="finish-page">
-		<v-container>
-			<v-row v-if="list">
-				<v-col>
-					<v-expansion-panels v-if="!loading && datamidtrans" class="mb-3" focusable>
-                        <v-expansion-panel
-                        v-for="(mid, i) in datamidtrans"
-                        :key="i"
-                        class="mb-3"
-                        >
-                            <v-expansion-panel-header>
-                                <v-row no-gutters>
-                                    <v-col cols="5 text--secondary">{{ '#' + mid.order_id }}</v-col>
-                                    <v-col cols="" class="text-center">{{ mid.transaction_time }}</v-col>
-                                </v-row>
-                            </v-expansion-panel-header>
-                            <v-expansion-panel-content class="pt-3">
-                                <v-subheader>Detail Pembayaran</v-subheader>
-                                <v-divider></v-divider>
-                                <v-simple-table>
-                                    <template v-slot:default>
-                                    <tbody>
-                                        <tr>
-                                            <td>Transfer Ke</td>
-                                            <td>BCA</td>
-                                        </tr>
-                                        <tr v-if="mid.veritrans_callback.va_numbers.length > 0">
-                                            <td>No. Virtual Account</td>
-                                            <td>
-                                                <v-row no-gutters v-for="(va, i) in mid.veritrans_callback.va_numbers" :key="i">
-                                                    <v-col cols="12">
-                                                        {{va.va_number}}
-                                                    </v-col>
-                                                </v-row>
-                                            </td>
-                                        </tr>
-                                        <tr v-else>
-                                            <td>No. Virtual Account</td>
-                                            <td> - </td>
-                                        </tr>
-                                        <tr>
-                                            <td>Nama Perusahan</td>
-                                            <td>PT. Jayadata Indonesia</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Jumlah Transfer</td>
-                                            <td>{{'Rp.' + mid.amount + ',-'}}</td>
-                                        </tr>
-                                    </tbody>
-                                    </template>
-                                </v-simple-table>
-                                <v-card>
-                                    <v-card-text>
-                                        <v-row v-if="mid.transaction_status == 'expire'">
-                                            <v-col cols="2">
-                                                <v-icon color="red" size="40">mdi-close-circle</v-icon>
-                                            </v-col>
-                                            <v-col cols="10">
-                                                <strong>
-                                                    Kami telah membatalkan transaksi ini, karena pembayaran tidak kami terima hingga batas waktu yang ditentukan hingga {{mid.expired_at}}
-                                                </strong>
-                                            </v-col>
-                                        </v-row>
-                                        <v-row v-else>
-                                            <v-col cols="2">
-                                                <v-icon color="green" size="40">mdi-check-circle</v-icon>
-                                            </v-col>
-                                            <v-col cols="10">
-                                                <strong>
-                                                    Pembayaran telah kami terima, VIP telah berhasil kami masukkan ke akun anda
-                                                </strong>
-                                            </v-col>
-                                            <v-col cols="12">
-                                                <!-- <v-btn @click="download()" depressed dark small block color="orange accent-14">DOWNLOAD INVOICE</v-btn> -->
-                                                <a :href="downloadUrl(mid.order_id)" target="blank" style="text-decoration: none" class="v-btn v-btn--block v-btn--depressed v-btn--tile theme--dark v-size--default deep-orange">DOWNLOAD INVOICE</a>
-                                            </v-col>
-                                        </v-row>
-                                    </v-card-text>
-                                </v-card>
-                            </v-expansion-panel-content>
-                        </v-expansion-panel>
-                    </v-expansion-panels>
-					<br>
-					<br>
-					<br>
-					<br>
-				</v-col>
-			</v-row>
-			<v-row v-else-if="!list && !loading">
-				<v-col>
-					<v-alert
-					prominent
-					text
-					type="info"
-					success>Tidak ada data yang tersedia</v-alert>
-				</v-col>
-			</v-row>
-			<v-skeleton-loader v-else
-			class="mx-auto mt-5"
-			type="list-item-avatar-three-line, list-item-avatar-three-line, list-item-avatar-three-line"
-			></v-skeleton-loader>
-		</v-container>
-	</section>
+  <section class="finish-page">
+    <v-container>
+      <v-row v-if="list">
+        <v-col>
+          <v-expansion-panels v-if="!loading && datamidtrans" class="mb-3" focusable>
+            <v-expansion-panel v-for="(mid, i) in datamidtrans" :key="i" class="mb-3">
+              <v-expansion-panel-header>
+                <v-row no-gutters>
+                  <v-col cols="4" class="text-secondary caption">{{ mid.transaction_time }}</v-col>
+                  <v-col
+                    cols="8"
+                    class="text-left"
+                  >Pembayaran No. {{ '#' + mid.order_id }} senilai {{ mid.amount }} telah kami terima</v-col>
+                </v-row>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content class="pt-3">
+                <v-subheader>Detail Pembayaran</v-subheader>
+                <v-divider></v-divider>
+                <v-simple-table>
+                  <template v-slot:default>
+                    <tbody>
+                      <tr>
+                        <td>Transfer Ke</td>
+                        <td>BCA</td>
+                      </tr>
+                      <tr v-if="mid.veritrans_callback.va_numbers.length > 0">
+                        <td>No. Virtual Account</td>
+                        <td>
+                          <v-row
+                            no-gutters
+                            v-for="(va, i) in mid.veritrans_callback.va_numbers"
+                            :key="i"
+                          >
+                            <v-col cols="12">{{va.va_number}}</v-col>
+                          </v-row>
+                        </td>
+                      </tr>
+                      <tr v-else>
+                        <td>No. Virtual Account</td>
+                        <td>-</td>
+                      </tr>
+                      <tr>
+                        <td>Nama Perusahan</td>
+                        <td>PT. Jayadata Indonesia</td>
+                      </tr>
+                      <tr>
+                        <td>Jumlah Transfer</td>
+                        <td>{{'Rp.' + mid.amount + ',-'}}</td>
+                      </tr>
+                    </tbody>
+                  </template>
+                </v-simple-table>
+                <!-- <v-card>
+                  <v-card-text>
+                    <v-row v-if="mid.transaction_status == 'expire'">
+                      <v-col cols="2">
+                        <v-icon color="red" size="40">mdi-close-circle</v-icon>
+                      </v-col>
+                      <v-col cols="10">
+                        <strong>Kami telah membatalkan transaksi ini, karena pembayaran tidak kami terima hingga batas waktu yang ditentukan hingga {{mid.expired_at}}</strong>
+                      </v-col>
+                    </v-row>
+                    <v-row v-else>
+                      <v-col cols="2">
+                        <v-icon color="green" size="40">mdi-check-circle</v-icon>
+                      </v-col>
+                      <v-col cols="10">
+                        <strong>Pembayaran telah kami terima, VIP telah berhasil kami masukkan ke akun anda</strong>
+                      </v-col>
+                      <v-col cols="12">
+                        <v-btn @click="download()" depressed dark small block color="orange accent-14">DOWNLOAD INVOICE</v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-card-text>
+                </v-card>-->
+                <div class="devider-small my-3"></div>
+                <a
+                  :href="downloadUrl(mid.order_id)"
+                  target="blank"
+                  style="text-decoration: none"
+                  class="v-btn v-btn--block v-btn--depressed theme--dark v-size--default deep-orange"
+                >DOWNLOAD INVOICE</a>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+          <br />
+          <br />
+          <br />
+          <br />
+        </v-col>
+      </v-row>
+      <v-row v-else-if="!list && !loading">
+        <v-col>
+          <v-alert prominent text type="info" success>Tidak ada data yang tersedia</v-alert>
+        </v-col>
+      </v-row>
+      <v-skeleton-loader
+        v-else
+        class="mx-auto mt-5"
+        type="list-item-avatar-three-line, list-item-avatar-three-line, list-item-avatar-three-line"
+      ></v-skeleton-loader>
+    </v-container>
+  </section>
 </template>
 <script>
-import UserService from '@/services/UserService'
+import UserService from "@/services/UserService";
 export default {
-	name:"Sukses",
-	data() {
-		return {
-            midtranscallback: [],
-            datamidtrans: [],
-            list: false,
-            loading: true
-		}
-	},
-	methods: {
-		async fetchData() {
-            try {
-                const res = await UserService.statusTransfer()
-                if (res.data.data.finish.length > 0) {
-                    this.list = true
-                }
-                this.loading = false
-                console.log(res)
-                this.datamidtrans = res.data.data.finish
-            } catch (error) {
-                console.log(error)
-            }
-        },
-        downloadUrl(order_id) {
-            return 'https://payment.playworld.id/api/midtrans/print/' + order_id
+  name: "Sukses",
+  data() {
+    return {
+      midtranscallback: [],
+      datamidtrans: [],
+      list: false,
+      loading: true
+    };
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const res = await UserService.statusTransfer();
+        if (res.data.data.finish.length > 0) {
+          this.list = true;
         }
-	},
-	mounted() {
-		this.fetchData()
-	}
-}
+        this.loading = false;
+        //console.log(res);
+        this.datamidtrans = res.data.data.finish;
+        console.log(JSON.parse(JSON.stringify(this.datamidtrans)));
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    downloadUrl(order_id) {
+      return "https://payment.playworld.id/api/midtrans/print/" + order_id;
+    }
+  },
+  mounted() {
+    this.fetchData();
+  }
+};
 </script>
-<style lang="sass" scoped>
-	.finish-page .container
-		background: #f9f9f9
+<style lang="scss">
+.finish-page .container {
+  background: #f9f9f9;
+}
+.finish-page .v-icon {
+  color: #ff5722 !important;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
 </style>
