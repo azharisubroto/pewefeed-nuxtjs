045645571d22a35f7de260e95f441c55f11f7cd7
Azhari Subroto <juarathemes@gmail.com>
2020-02-14 14:46:20 +0700

    mpw revisi thumbnail tukar poin

--- components/TukarPoin/TukarPoin.vue
+++ components/TukarPoin/TukarPoin.vue
@@ -1,185 +1,195 @@
 <template>
-    <section class="pb-5">
-      <!-- LATEST -->
-      <flickity ref="flashPoin" :options="flickityOptions">
-        <div
-        v-for="article in flashpoints"
-        :key="article.redeem.id"
-        @click="$router.push('/toko/redeem/'+article.redeem.id)"
-        class="featured-item-2">
-          <v-btn outlined rounded small class="gray poinbutuh">
-            <img src="/img/poin.png" alt="" width="16" class="mr-1"> <strong>{{ article.redeem.point ? article.redeem.point : 'Closed' }}</strong>
-          </v-btn>
-          <v-img
-            :contain="true"
-            :src="article.redeem.image ? article.redeem.image : ''"
-            class="grey lighten-2"
-          ></v-img>
+  <section class="pb-5">
+    <!-- LATEST -->
+    <flickity v-if="flashpoints" ref="flashPoin" :options="flickityOptions">
+      <div v-for="item in flashpoints" :key="item.redeem.id" class="featured-item-2">
+        <div class="pa-3 orange lighten-2">
+          <v-row align="center">
+            <v-col cols="4">
+              <v-img :contain="true" :src="item.redeem.image ? item.redeem.image : ''"></v-img>
+            </v-col>
+            <v-col cols="8">
+              <div class></div>
+              <img src="/img/poin.png" alt width="16" class="mr-1 mt-1" style="vertical-align:top" />
+              <strong class="text-14" style="line-height:1">{{item.redeem.point}}</strong>
+              <h1 class="mb-2">{{item.title}}</h1>
+              <v-btn
+                color="deep-orange"
+                depressed
+                dark
+                @click="$router.push('/toko/redeem/'+item.redeem.id)"
+              >Tukar Poin</v-btn>
+            </v-col>
+          </v-row>
         </div>
-      </flickity>
+      </div>
+    </flickity>
 
-      <div class="devider-small"></div>
+    <div class="devider-small"></div>
 
-      <v-container class="mt-5 pt-5 mb-5 pb-5">
-        <template v-if="tukarpointab">
-          <v-row
-            class="topview-item"
-            v-for="(article, i) in redeems"
-            :key="'topview-'+article.id+'-'+i"
-            @click="$router.push('/toko/redeem/'+article.id)"
-          >
+    <v-container class="mt-5 pt-5 mb-5 pb-5">
+      <template v-if="tukarpointab">
+        <v-row
+          class="topview-item"
+          v-for="(article, i) in redeems"
+          :key="'topview-'+article.id+'-'+i"
+          @click="$router.push('/toko/redeem/'+article.id)"
+        >
           <v-col cols="12" v-if="i%5 == 0">
             <!-- ADSENSE -->
             <InFeedAdsense
               data-ad-layout-key="-fb+5w+4e-db+86"
               data-ad-client="ca-pub-6581994114503986"
-              data-ad-slot="7916120444">
-            </InFeedAdsense>
+              data-ad-slot="7916120444"
+            ></InFeedAdsense>
           </v-col>
-            <v-col cols="4">
-              <v-img
-                :contain="true"
-                :src="article.image"
-                aspect-ratio="1"
-                class="grey lighten-2 background-white"
-              >
-              </v-img>
-            </v-col>
-            <v-col cols="8" class="d-flex align-content-space-between flex-wrap">
-                <h2 class="d-block" style="width:100%;">{{article.reward}}</h2>
-                <div class="self-align-end meta text--gray" style="font-size:16px;">
-                  <v-btn outlined rounded small class="gray">
-                    <img src="/img/poin.png" alt="" width="16" class="mr-1"> <strong>{{article.point}}</strong>
-                  </v-btn>
-                </div>
-            </v-col>
-          </v-row>
+          <v-col cols="4">
+            <div class="orange lighten-2 pa-3">
+              <v-img contain :src="article.image" aspect-ratio="1" class="orange lighten-2"></v-img>
+            </div>
+          </v-col>
+          <v-col cols="8" class="d-flex align-content-space-between flex-wrap">
+            <div>
+              <h2 class="d-block mb-2" style="width:100%;">{{article.reward}}</h2>
+              <img src="/img/poin.png" alt width="16" class="mr-1 mt-1" style="vertical-align:top" />
+              <strong class="text-14" style="line-height:1">{{article.point}}</strong>
+            </div>
+            <div class="self-align-end meta text--gray" style="font-size:16px;">
+              <v-btn
+                color="deep-orange"
+                depressed
+                dark
+                small
+                @click="$router.push('/toko/redeem/'+item.redeem.id)"
+              >Tukar Poin</v-btn>
+            </div>
+          </v-col>
+        </v-row>
 
-          <v-row class="mt-4">
-            <v-col>
-              <v-pagination
-              v-model="page"
-              :length="totalpage"
-              color="orange"
-              @input="next"
-              ></v-pagination>
-            </v-col>
-          </v-row>
-        </template>
+        <v-row class="mt-4">
+          <v-col>
+            <v-pagination v-model="page" :length="totalpage" color="orange" @input="next"></v-pagination>
+          </v-col>
+        </v-row>
+      </template>
 
-        <template v-if="syarattab">
-          <v-row class="mt-0">
-            <v-col>
-              <h2 class="mb-4">Syarat &amp; Ketentuan</h2>
-              <p>Untuk mendapatkan barang cukup menukarkan POIN sesuai dengan jumlah POIN YANG DIPERLUKAN</p>
+      <template v-if="syarattab">
+        <v-row class="mt-0">
+          <v-col>
+            <h2 class="mb-4">Syarat &amp; Ketentuan</h2>
+            <p>Untuk mendapatkan barang cukup menukarkan POIN sesuai dengan jumlah POIN YANG DIPERLUKAN</p>
 
-              <p>Untuk mendapatkan POIN lakukan SHARE, COMMENT, jawab QUIZ disetiap Artikel PLAYWORLD.ID atau mainkan GAME nya.</p>
+            <p>Untuk mendapatkan POIN lakukan SHARE, COMMENT, jawab QUIZ disetiap Artikel PLAYWORLD.ID atau mainkan GAME nya.</p>
 
-              <p>Cek videonya disini :</p>
+            <p>Cek videonya disini :</p>
 
-              <iframe width="320" height="315" src="https://www.youtube.com/embed/_gbe_xq27pE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </v-col>
-          </v-row>
-        </template>
-      </v-container>
+            <iframe
+              width="320"
+              height="315"
+              src="https://www.youtube.com/embed/_gbe_xq27pE"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </v-col>
+        </v-row>
+      </template>
+    </v-container>
 
+    <v-bottom-navigation fixed dark grow color="white" background-color="black">
+      <v-btn @click="tukarpointab=true;syarattab=false">
+        <span>Tukar Poin</span>
+      </v-btn>
 
-      <v-bottom-navigation
-        fixed
-        dark
-        grow
-        color="white"
-        background-color="black"
-      >
-        <v-btn @click="tukarpointab=true;syarattab=false">
-          <span>Tukar Poin</span>
-        </v-btn>
-
-        <v-btn @click="tukarpointab=false;syarattab=true">
-          <span>Syarat &amp; Ketentuan</span>
-        </v-btn>
-      </v-bottom-navigation>
-    </section>
+      <v-btn @click="tukarpointab=false;syarattab=true">
+        <span>Syarat &amp; Ketentuan</span>
+      </v-btn>
+    </v-bottom-navigation>
+  </section>
 </template>
 
 <script>
-import TukarPoinService from '@/services/TukarPoinService'
+import TukarPoinService from "@/services/TukarPoinService";
 export default {
-    name:"PwPoin",
-    data() {
-        return {
-            tukarpointab: true,
-            syarattab: false,
-            toppoinbanner: 'http://b16e2bab9e94a9d05089-aa7428b954372836cd8898750ce2dd71.r41.cf6.rackcdn.com/assets/frontend/images/banner-toppoin.jpg',
-            model: null,
-            redeems: null,
-            flashpoints: null,
-            garfik: '',
-            tukarpoin: '',
-            totalpage: 1,
-            page: 1,
-            flickityOptions: {
-              groupCells: 1,
-              prevNextButtons: false,
-              pageDots: true,
-              wrapAround: true
-            }
-        }
-    },
-    methods: {
-      async fetchRedeemItems(n) {
-        var n = n ? n : 1
-        var params = {
-          page: 1
-        }
-        try {
-          const res = await TukarPoinService.getRedeemItems(n)
-          console.log(JSON.parse(JSON.stringify(res)))
-          this.redeems = res.data.data
-          this.totalpage = res.data.pagination.last_page
-        } catch (error) {
-            console.log(error)
-        }
-      },
-      async fetchFlashPoint() {
-        try {
-          const res = await TukarPoinService.getFlashPoint()
-          console.log(JSON.parse(JSON.stringify(res.data.flash_point)))
-          this.flashpoints = res.data.flash_point
-          // if (this.flashpoints.length > 0){
-            this.$nextTick(function() {
-              this.$refs.flashPoin.rerender();
-            });
-          // }
-        } catch (error) {
-          console.log(error)
-        }
-      },
-      next(num) {
-        this.fetchRedeemItems(num)
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        });
+  name: "PwPoin",
+  data() {
+    return {
+      tukarpointab: true,
+      syarattab: false,
+      toppoinbanner:
+        "http://b16e2bab9e94a9d05089-aa7428b954372836cd8898750ce2dd71.r41.cf6.rackcdn.com/assets/frontend/images/banner-toppoin.jpg",
+      model: null,
+      redeems: null,
+      flashpoints: null,
+      garfik: "",
+      tukarpoin: "",
+      totalpage: 1,
+      page: 1,
+      flickityOptions: {
+        groupCells: 1,
+        prevNextButtons: false,
+        pageDots: true,
+        wrapAround: true
+      }
+    };
+  },
+  methods: {
+    async fetchRedeemItems(n) {
+      var n = n ? n : 1;
+      var params = {
+        page: 1
+      };
+      try {
+        const res = await TukarPoinService.getRedeemItems(n);
+        console.log(JSON.parse(JSON.stringify(res)));
+        this.redeems = res.data.data;
+        this.totalpage = res.data.pagination.last_page;
+      } catch (error) {
+        console.log(error);
       }
     },
-    mounted(){
-      this.fetchRedeemItems()
-      this.fetchFlashPoint()
+    async fetchFlashPoint() {
+      try {
+        const res = await TukarPoinService.getFlashPoint();
+        console.log(
+          "flash points:",
+          JSON.parse(JSON.stringify(res.data.flash_point))
+        );
+        this.flashpoints = res.data.flash_point;
+        // if (this.flashpoints.length > 0){
+        this.$nextTick(function() {
+          this.$refs.flashPoin.rerender();
+        });
+        // }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    next(num) {
+      this.fetchRedeemItems(num);
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      });
     }
-}
+  },
+  mounted() {
+    this.fetchRedeemItems();
+    this.fetchFlashPoint();
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  .featured-item-2 {
-    width: 100%;
-    height:200px;
-  }
-  .poinbutuh {
-    position: absolute;
-    z-index: 100;
-    right: 10px;
-    top: 10px;
-  }
+.featured-item-2 {
+  width: 100%;
+  height: 200px;
+}
+.poinbutuh {
+  position: absolute;
+  z-index: 100;
+  right: 10px;
+  top: 10px;
+}
 </style>
