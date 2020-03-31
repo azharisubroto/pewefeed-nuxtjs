a2b97520f59f90965b9996ba09d5ce6190f9d626
Azhari Subroto <juarathemes@gmail.com>
2020-03-31 15:04:12 +0700

    login icon

--- components/Login.vue
+++ components/Login.vue
@@ -1,67 +1,73 @@
 <template>
-    <div class="container">
-        <h3 class="my-3" id="loginhead">LOGIN / SIGNUP</h3>
-        <OAuth provider="facebook" btnclass="indigo darken-1 " @makeloading="setloading" @notloading="notloading"/>
-        <OAuth provider="google" btnclass="red darken-1" @makeloading="setloading" @notloading="notloading"/>
+  <div class="container">
+    <h3 class="my-3" id="loginhead">LOGIN / SIGNUP</h3>
+    <OAuth
+      provider="facebook"
+      btnclass="indigo darken-1 "
+      @makeloading="setloading"
+      @notloading="notloading"
+    />
+    <OAuth
+      provider="google"
+      btnclass="red darken-1"
+      @makeloading="setloading"
+      @notloading="notloading"
+    />
 
-        <v-overlay :value="overlay">
-            <v-progress-circular indeterminate size="64"></v-progress-circular>
-        </v-overlay>
+    <v-overlay :value="overlay">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
 
-        <v-dialog
-            v-model="fail"
-            max-width="290"
-        >
-            <v-card>
-                <v-card-title class="headline justify-center"><div id="gagal-login" style="color: red">Gagal Login!</div></v-card-title>
+    <v-dialog v-model="fail" max-width="290">
+      <v-card>
+        <v-card-title class="headline justify-center">
+          <div id="gagal-login" style="color: red">Gagal Login!</div>
+        </v-card-title>
 
-                <v-card-text align="center">
-                    <v-img
-                        src="@/assets/img/sad.svg"
-                        width="100"
-                    ></v-img>
-                    <span id="text-maaf">Kami tidak dapat menemukan akun anda dalam database kami, silahkan mencoba dengan akun facebook/google yang berbeda</span>
-                </v-card-text>
+        <v-card-text align="center">
+          <v-img src="/img/sad.png" class="my-3" width="100"></v-img>
+          <span
+            id="text-maaf"
+          >Kami tidak dapat menemukan akun anda dalam database kami, silahkan mencoba dengan akun facebook/google yang berbeda</span>
+        </v-card-text>
 
-                <v-card-actions>
-                    <v-btn
-                        color="deep-orange accent-2"
-                        depressed
-                        dark
-                        block
-                        id="tutupbtn"
-                        @click="fail = false"
-                    >
-                        Oke
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-    </div>
+        <v-card-actions>
+          <v-btn
+            color="deep-orange accent-2"
+            depressed
+            dark
+            block
+            id="tutupbtn"
+            @click="fail = false"
+          >Oke</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
 import OAuth from "@/components/OAuth";
 
 export default {
-    name: "Login",
-    components: {
-      OAuth
+  name: "Login",
+  components: {
+    OAuth
+  },
+  data() {
+    return {
+      overlay: false,
+      fail: false
+    };
+  },
+  methods: {
+    setloading() {
+      this.overlay = true;
     },
-    data(){
-        return {
-            overlay: false,
-            fail: false
-        }
-    },
-    methods:{
-        setloading () {
-            this.overlay = true
-        },
-        notloading() {
-            this.overlay = false
-            this.fail = true
-        }
+    notloading() {
+      this.overlay = false;
+      this.fail = true;
     }
-  };
+  }
+};
 </script>
--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/sw.js?1584682876904', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/sw.js?1585641611260', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
