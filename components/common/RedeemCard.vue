89a403a1ccb8fd69dc91f711a6042e5ddf4509ca
Riza Alif Wildani <rizaalifdmc@gmail.com>
2020-04-19 12:20:10 +0700

    RedeemCard

--- components/common/RedeemCard.vue
+++ components/common/RedeemCard.vue
@@ -1,29 +1,28 @@
 <template>
     <v-card class="mt-4" elevation="0" v-if="data" @click="$router.push('/tukarpoin/redeem/'+data.redeem.id)">
         <div>
-            <div :class="['orange jangan lighten-1 py-2', 'px-7']">
+            <div :class="['orange jangan lighten-1 py-1', 'px-4']">
               <v-row align="center" class="py-0">
                   <v-col cols="4">
-                      <v-img :aspect-ratio="1/2" :src="data.redeem.image" contain></v-img>
+                      <v-img :aspect-ratio="2/3" :src="data.redeem.image" contain></v-img>
                   </v-col>
                   <v-col cols="8">
                       <v-row>
-                          <v-col cols="2" style="margin-top: -5px !important">
+                          <v-col cols="3" style="margin-top: -5px !important" justify="center" align="center">
                               <v-img
                                   src="https://be2ad46f1850a93a8329-aa7428b954372836cd8898750ce2dd71.ssl.cf6.rackcdn.com/assets/frontend/img/m-menu2/p.png"
-                                  width="30"
-                                  class="mt-1"
+                                  width="20"
                               >
                               </v-img>
                           </v-col>
-                          <v-col cols="10" style="margin-left: -15px" justify="center" align="left" class="pt-0">
+                          <v-col cols="9" style="margin-left: -15px" justify="center" align="left" class="pt-0">
                                 <div class="text-20">
                                   <strong class="text-14">{{ data.redeem.price }}</strong>
                                 </div>
                                 <template>
                                   <strong class="text-18">{{ data.redeem.title }}</strong>
                                   <div></div>
-                                  <v-btn class="mt-3" color="deep-orange" dark depressed>TUKAR POIN</v-btn>
+                                  <v-btn class="mt-3" small color="deep-orange" dark depressed>TUKAR POIN</v-btn>
                                 </template>
                           </v-col>
                       </v-row>
