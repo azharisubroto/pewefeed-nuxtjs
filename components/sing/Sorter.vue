5edb7002df25a16f07925955ec607f589ca96b74
Azhari Subroto <juarathemes@gmail.com>
2020-07-21 19:24:15 +0700

    update sore aja

--- components/sing/Sorter.vue
+++ components/sing/Sorter.vue
@@ -1,36 +1,5 @@
 <template>
-  	<v-bottom-sheet v-model="intDialogVisible">
-      <v-sheet height="100%">
-        <v-toolbar :elevation="1">
-          <!-- Arrow -->
-          <v-btn
-            dark
-            icon
-            tile
-            style="border-right: 0px solid #717171"
-            light
-            @click="intDialogVisible = false;"
-          >
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
 
-          <!-- Title -->
-          <div class="flex-grow-1"></div>
-          <v-toolbar-items>
-            <v-btn dark text class="deep-orange--text pl-0 text-uppercase" style="margin-left:-10px;">SORT</v-btn>
-          </v-toolbar-items>
-          <div class="flex-grow-1"></div>
-        </v-toolbar>
-
-        <div class="mx-0 text-center px-4">
-			<template v-for="(item, i) in sorter">
-				<div class="devider-small" :key="'devtop-'+i"></div>
-				<div class="py-4" :key="'devmid-'+i">{{item}}</div>
-				<div class="devider-small" :key="'devbot-'+i"></div>
-			</template>
-        </div>
-      </v-sheet>
-    </v-bottom-sheet>
 </template>
 
 <script>
