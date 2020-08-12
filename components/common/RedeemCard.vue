ab31f914319ee2c77b0d13b380fe0369383d39ed
Azhari Subroto <juarathemes@gmail.com>
2020-08-12 10:51:21 +0700

    revisi tukarpoin home and detaul

--- components/common/RedeemCard.vue
+++ components/common/RedeemCard.vue
@@ -1,64 +1,76 @@
 <template>
-    <v-card class="mt-4" elevation="0" v-if="data" @click="$router.push('/tukarpoin/redeem/'+data.redeem.id)">
-        <div>
-            <div :class="['orange jangan lighten-1 py-1', 'px-4']">
-              <v-row align="center" class="py-0">
-                  <v-col cols="4">
-                      <v-img :aspect-ratio="2/3" :src="data.redeem.image" contain></v-img>
-                  </v-col>
-                  <v-col cols="8">
-                      <v-row>
-                          <v-col cols="3" style="margin-top: -5px !important" justify="center" align="center">
-                              <v-img
-                                  src="https://be2ad46f1850a93a8329-aa7428b954372836cd8898750ce2dd71.ssl.cf6.rackcdn.com/assets/frontend/img/m-menu2/p.png"
-                                  width="20"
-                              >
-                              </v-img>
-                          </v-col>
-                          <v-col cols="9" style="margin-left: -15px" justify="center" align="left" class="pt-0">
-                                <div class="text-20">
-                                  <strong class="text-14">{{ data.redeem.price }}</strong>
-                                </div>
-                                <template>
-                                  <strong class="text-18">{{ data.redeem.title }}</strong>
-                                  <div></div>
-                                  <v-btn class="mt-3" small color="deep-orange" dark depressed>TUKAR POIN</v-btn>
-                                </template>
-                          </v-col>
-                      </v-row>
-                  </v-col>
-              </v-row>
-            </div>
-        </div>
-    </v-card>
+  <v-card
+    class="mt-4"
+    elevation="0"
+    v-if="data"
+    @click="$router.push('/tukarpoin/redeem/'+data.redeem.id)"
+  >
+    <div>
+      <div :class="['orange jangan lighten-1 py-1', 'px-4']">
+        <v-row align="center">
+          <v-col cols="3">
+            <img :src="data.redeem.image" contain>
+          </v-col>
+          <v-col cols="9" class="pr-4">
+            <v-row no-gutters class="mx-0">
+              <v-col cols="2" class="pr-2">
+                <img
+                  src="/img/icons/poin-black.svg"
+                  alt
+                  class="mr-1 mt-1"
+				  width="26"
+                  style="vertical-align:top"
+                />
+              </v-col>
+              <v-col cols="10">
+                <strong class="text-20 black--text d-block mt-1" style="line-height:1">{{data.redeem.price}} Poin</strong>
+              </v-col>
+            </v-row>
+            <div class="devider-small my-3" style="border-color:#000"></div>
+
+            <div class="text-18 black--text mb-2">{{data.redeem.title}}</div>
+
+			<v-btn
+			color="deep-orange"
+			depressed
+			dark
+			block
+			small
+			@click="$router.push('/tukarpoin/redeem/'+data.redeem.id)"
+			>Tukar Poin</v-btn>
+          </v-col>
+        </v-row>
+      </div>
+    </div>
+  </v-card>
 </template>
 
 <script>
 import RedeemService from "@/services/RedeemService";
 export default {
-    name:"RedeemCard",
-    data() {
-        return {
-            data: null      
-        }
+  name: "RedeemCard",
+  data() {
+    return {
+      data: null,
+    };
+  },
+  methods: {
+    numberWithCommas(x) {
+      // console.log(x)
+      // return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     },
-    methods:{
-        numberWithCommas(x) {
-            // console.log(x)
-            // return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-        },
-        async getRedeem() {
-            try {
-                const res = await RedeemService.getRedeem();
-                // console.log(res.data.data.redeem)
-                this.data = res.data.data.redeem;
-            } catch (error) {
-                console.log(error);
-            }
-        }
+    async getRedeem() {
+      try {
+        const res = await RedeemService.getRedeem();
+        // console.log(res.data.data.redeem)
+        this.data = res.data.data.redeem;
+      } catch (error) {
+        console.log(error);
+      }
     },
-    mounted() {
-        this.getRedeem();
-    }
-  }
+  },
+  mounted() {
+    this.getRedeem();
+  },
+};
 </script>
