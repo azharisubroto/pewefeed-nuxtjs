7801a3e391a9f7ae65966d4e31e18cb21a6a2bfe
Azhari Subroto <juarathemes@gmail.com>
2020-09-01 15:28:11 +0700

    change prize items to banner image

--- components/sing/SingPrizes.vue
+++ components/sing/SingPrizes.vue
@@ -4,7 +4,8 @@
 			<div class="text-18" style="color: #000"><strong>Pemenang Pilihan Juri</strong></div>
 		</v-container>
 		<template v-for="(item, i) in prizes">
-			<v-card color="#404040" class="px-2 my-4 mx-3" v-if="i<3"  :key="'prize-'+i">
+			<v-img :key="'prizeup-'+i" class="px-2 my-4 mx-3" v-if="i<3" :src="'/img/banner/JOGED-CHALLENGE-PRIZES-'+parseInt(i+1)+'.jpg'"></v-img>
+			<!-- <v-card color="#404040" class="px-2 my-4 mx-3" v-if="i<3"  :key="'prize-'+i">
 				<v-row>
 					<v-col cols="3">
 						<v-img v-if="item.redeem" :src="item.redeem.image"></v-img>
@@ -16,36 +17,16 @@
 						<div class="text-16 mt-4" >
 							{{item.redeem.title}}
 						</div>
-						<!-- <h3 class="text-18 mt-2" >
-							{{item.total_winners}} Pemenang
-						</h3> -->
 					</v-col>
 				</v-row>
-			</v-card>
+			</v-card> -->
 		</template>
 
 		<v-container class="py-3">
 			<div class="text-18"><strong>Pemenang Vote Terbanyak</strong></div>
 		</v-container>
-		<template v-for="(item, i) in prizes">
-			<v-card color="#404040" v-if="i>=3" class="px-2 my-4 mx-4" :key="'prize2-'+i">
-				<v-row>
-					<v-col cols="3">
-						<v-img v-if="item.redeem" :src="item.redeem.image"></v-img>
-					</v-col>
-					<v-col cols="9" class="pt-3 ">
-						<div class="text-16" style="margin-top:4px">
-							<img src="/img/trophy.svg" alt="" width="16" style="vertical-align:middle;margin-left:1px" class="mr-1"/> <strong>Juara {{i+1}}</strong>
-						</div>
-						<div class="text-16 mt-4" >
-							{{item.redeem.title}}
-						</div>
-						<!-- <h3 class="text-18 mt-2" >
-							{{item.total_winners}} Pemenang
-						</h3> -->
-					</v-col>
-				</v-row>
-			</v-card>
+		<template v-for="i in 3">
+			<v-img :key="'prizebot-'+i" class="px-2 my-4 mx-3" :src="'/img/banner/JOGED-CHALLENGE-PRIZES-'+parseInt(i+3)+'.jpg'"></v-img>
 		</template>
 	</div>
 </template>
